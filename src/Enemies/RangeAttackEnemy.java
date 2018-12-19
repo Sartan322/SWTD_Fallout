f899a198bb07ad83901426a26b5bacee839783ae
Pavel Prokopev <Bangarang777@mail.ru>
2018-12-19 12:59:56 +0300

    Dog

--- src/Enemies/RangeAttackEnemy.java
+++
@@ -1,8 +0,0 @@
-package Enemies;
-
-public abstract class RangeAttackEnemy extends Enemy {
-    public RangeAttackEnemy(String name, int hp, int damage) {
-        super(name, hp, damage);
-    }
-    public abstract int attack();
-}
